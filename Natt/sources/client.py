972de2457093afa9881cef741fb16c55b97c3d81
neo <neotoxic.off@gmail.com>
2024-04-25 18:29:11 +0200

    fix and make it work with buffer

--- Natt/sources/client.py
+++ Natt/sources/client.py
@@ -9,35 +9,49 @@ class DiscordClient:
         self.scrapper = Scrapper()
         self.whitelist: list = whitelist
 
+    def __is_whitelisted__(self, item_title):
+        for item in self.whitelist:
+            if (item.lower() in item_title.lower()):
+                return (True)
+        return (False)
+
     def run(self):
         self.scrapper.get()
-
+        embeds = []
         headers = {
             "Content-Type": "application/json"
         }
+        messages = []
         
         for item in self.scrapper.anime:
-            payload = {
-                "embeds": [
-                    {
-                        "title": item["title"],
-                        "url": item["link"],
-                        "color": 0xce00ff,
-                        "thumbnail": {"url": "https://upload.wikimedia.org/wikipedia/commons/a/a0/Nyaa_Logo.png"},
-                        "fields": [
-                            {"name": "Publication Date", "value": item["pub_date"], "inline": False},
-                            {"name": "Category", "value": item["category"], "inline": False},
-                            {"name": "Seeders", "value": item["seeders"], "inline": True},
-                            {"name": "Leechers", "value": item["leechers"], "inline": True},
-                            {"name": "Downloads", "value": item["downloads"], "inline": True},
-                            {"name": "Size", "value": item["size"], "inline": False}
-                        ]
-                    }
-                ]
-            }
-        response = requests.post(os.environ.get("DISCORD_WEBHOOKS"), data=json.dumps(payload), headers=headers)
+            if ("anime" in item["category"].lower() and self.__is_whitelisted__(item["title"])):
+                if (len(embeds) == 10):
+                    messages.append(embeds.copy())
+                    embeds.clear()
 
-        if response.status_code == 200:
-            print("Webhook sent successfully!")
-        else:
-            print(f"Failed to send webhook. Status code: {response.status_code}")
+                embeds.append({
+                    "title": item["title"],
+                    "url": item["link"],
+                    "color": 0xce00ff,
+                    "thumbnail": {"url": "https://upload.wikimedia.org/wikipedia/commons/a/a0/Nyaa_Logo.png"},
+                    "fields": [
+                        {"name": "Publication Date", "value": item["pub_date"], "inline": False},
+                        {"name": "Category", "value": item["category"], "inline": False},
+                        {"name": "Seeders", "value": item["seeders"], "inline": True},
+                        {"name": "Leechers", "value": item["leechers"], "inline": True},
+                        {"name": "Downloads", "value": item["downloads"], "inline": True},
+                        {"name": "Size", "value": item["size"], "inline": False}
+                    ]
+                })
+
+        for i in range(0, len(messages)):
+            payload = {
+                "embeds": messages[i]
+            }
+
+            response = requests.post(os.environ.get("DISCORD_WEBHOOKS"), data=json.dumps(payload), headers=headers)
+
+            if response.status_code == 200:
+                print("Webhook sent successfully!")
+            else:
+                print(f"Failed to send webhook. Status code: {response.status_code} {response.text}")
